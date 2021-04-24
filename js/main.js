ebb0df3f3023cd6bf2bf608dfd6bf9143cb08680
Raphaël Deschler <raphael.deschler@gmail.com>
2021-04-25 00:49:17 +0200

    Print banner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Vars
+const banner = `
+Initialisation du terminal ...
+Chargement du profil : Raphaël Deschler ...
+...
+Vous êtes maintenant connecté ...
+Tapez "aide" pour vous les commandes disponibles ...
+`
+const buflen = 8
+const tickrate = 1000 / 60
+const prompt = () => '$ > '
+
 // Textarea
 const createElement = root => {
     const el = document.createElement('textarea')
@@ -10,16 +22,70 @@ const createElement = root => {
     return el
 }
 
+// Creates the rendering loop
+const renderer = (tickrate, onrender) => {
+    let lastTick = 0
+
+    const tick = (time) => {
+      const now = performance.now()
+      const delta = now - lastTick
+
+      if (delta > tickrate) {
+        lastTick = now - (delta % tickrate)
+
+        onrender()
+      }
+
+      window.requestAnimationFrame(tick)
+    }
+
+    return tick
+}
+
+// Printer
+const printer = ($element, buflen) => buffer => {
+    if (buffer.length > 0) {
+      const len = Math.min(buflen, buffer.length)
+      const val = buffer.splice(0, len)
+
+      $element.value += val.join('')
+      $element.scrollTop = $element.scrollHeight
+
+      return true
+    }
+
+    return false
+}
+
 // Terminal
-const terminal = () => {
+const terminal = (banner, buflen, tickrate, prompt) => {
+    let buffer = [] // What will be displayed
+    let busy = false // If we cannot type at the moment
+
     const $root = document.querySelector('#terminal')
     const $element = createElement($root)
     $root.appendChild($element)
+
+    const output = (output) => {
+        let append = output + '\n' + prompt();
+        buffer = buffer.concat(append.split(''))
+    }
+
+    const print = printer($element, buflen)
+    const onrender = () => (busy = print(buffer))
+    const render = renderer(tickrate, onrender)
+
+    render()
+    output(banner)
+
+    return {
+        print: output
+    }
 }
 
 // Onload
 const load = () => {
-    const t = terminal()
+    const t = terminal(banner, buflen, tickrate, prompt)
 }
 
 document.addEventListener('DOMContentLoaded', load)
